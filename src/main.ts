b01513588309a522d8443122e6ea7b070d90a887
Paficent <paficent@tutamail.com>
2026-05-25 22:05:59 -0400

    Removed contextmenu

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import "./app.css";
 import App from "./App.svelte";
 import { mount } from "svelte";
 
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
 const app = mount(App, { target: document.getElementById("app")! });
 
 export default app;
